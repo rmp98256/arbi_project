0813c19e733f887f6ca44c30aef74b9b870315f7
Patel <mapatel@ptc.com>
2025-02-02 16:53:04 +0530

    updating jai balram

---
+++ GuiTest/src/ColumnConstants.java
@@ -0,0 +1,24 @@
+
+public class ColumnConstants {
+	String[] columnNames = { "Former Name", "No. of Bags", "Weight (40/50 kg)", "Rate per Bag", "Washed",
+			"Loading Cost", "Rate per Kg", "Net Weight", "Cost of Vegetables" };
+	
+	public static int FOMER_NAME=0;
+	
+	public static int NO_OF_BAGS=1;
+	
+	public static int WEIGHT_OF_BAG=2;
+	
+	public static int RATE_PER_BAG=3;
+	
+	public static int WASHED=4;
+	
+	public static int LOADING_COST=5;
+	
+	public static int RATE_PER_KG=6;
+	
+	public static int NET_WEIGHT=7;
+	
+	public static int COST_OF_VEGETABLE=8;
+	
+}
---
+++ GuiTest/src/Constants.java
@@ -0,0 +1,16 @@
+
+public class Constants {
+	public static String ARBI="Arbi";
+	
+	public static String ADARAK="Adarak";
+	
+	public static String WASHINGANDLOADING="Washing and Loading";
+	
+	public static String WASHING="Washing";
+	
+	public static String LOADING="Loading";
+	
+	public static String NONOFTHEABOVE="Non of the above";
+	
+
+}
